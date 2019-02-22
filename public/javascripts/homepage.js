dd227a8db60583ff8d9d298281602750a526e3f1
AdityaBongale <ad1tya2b2@gmail.com>
2019-02-22 15:12:33 -0500

    Animated Party and Char Names

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+    $('.partybtn').on('click',function(){
+        console.log('search');
+        if ($('.partyname').hasClass('hidden')) {
+            $('.partyname').removeClass('hidden');
+            $('.partyname').addClass('visible');
+        } else {
+            $('.partyname').removeClass('visible');
+            $('.partyname').addClass('hidden');
+        }
 
+    });
+    $('.charbtn').on('click',function(){
+        console.log('user');
+        if ($('.charname').hasClass('hidden')) {
+            $('.charname').removeClass('hidden');
+            $('.charname').addClass('visible');
+        } else {
+            $('.charname').removeClass('visible');
+            $('.charname').addClass('hidden');
+        }
+    });
+    $('.votebtn').on('click',function(){
+        console.log('vote');
+    });
 });
