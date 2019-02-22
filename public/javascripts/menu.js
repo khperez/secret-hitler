30c9b9168c4e801e4edb99adf4c116d1513c6270
AdityaBongale <ad1tya2b2@gmail.com>
2019-02-22 14:48:22 -0500

    Centered buttons on start page

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,12 +1,4 @@
-$('#plusButton').click(function () {
-    $('.plusVer').toggleClass('plusVer_active');
-    $('.picture').toggleClass('picture_up');
-    $('.options').toggle();
-    $('.options').toggleClass('options_show');
-    $('.nav_btns').toggleClass('nav_btns_hide');
-  });
-
-  $(document).ready(function() {
+$(document).ready(function() {
     $("i.fa.fa-bars.fa-2x").on("click", function() {
           $("nav ul").toggleClass("showing");
     });
