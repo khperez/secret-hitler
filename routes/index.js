1734604ed18b81730322ba02d5fabe2190ad226a
Katherine Perez <perezkh@gmail.com>
2019-02-22 19:07:19 -0800

    add alpha start page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('start', { title: 'Secret Hilter' });
+  res.render('start', { title: 'Secret Hitler' });
 });
 
 router.get('/home', function(req, res, next) {
