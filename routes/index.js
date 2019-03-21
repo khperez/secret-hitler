b41a485ec91edbf2dde8f050a834d16e368c0383
Katherine Perez <perezkh@gmail.com>
2019-03-21 15:32:58 -0700

    combine styles across app; refactor layout pages
    main.pug
    |- start.pug
    |- game.pug
    |- help.pug

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('start', { title: 'Secret Hitler' });
 });
 
-router.get('/home', function(req, res, next) {
-  res.render('homepage', { title: 'Home' });
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Game' });
+});
+router.get('/help', function(req, res, next) {
+  res.render('help', { title: 'Secret Hitler'});
 });
 
 module.exports = router;
