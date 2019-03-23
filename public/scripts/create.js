f14a42f2d90d09b3d4f3b2f32175a1a3fd882f3c
Katherine Perez <perezkh@gmail.com>
2019-03-22 18:00:42 -0700

    viewport fix (maybe -- part. 2)

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -5,18 +5,9 @@ names = ["Winston Churchill",
          "Benito Mussolini"]
 
 $(document).ready(function() {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     $("i.fas.fa-random").on("click", function() {
         document.getElementById("create-name").value = getRandomPerson();
-    });
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
 });
 
---
+++ public/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    let vh = window.innerHeight * 0.01;
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+});
